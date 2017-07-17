46c5a855c2c814a036b754377b087035958a3967
Sinan ACAR <sinanacar14654@gmail.com>
2017-07-17 22:23:54 +0300

    Add files via upload

---
+++ karakok alma programı/karakok alma programı/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace üs_alma_karakok_alma_programı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double karakök;
+
+            karakök =Convert.ToDouble(textBox1.Text);
+           
+            karakök = Math.Sqrt(karakök);
+            label4.Text = karakök.ToString();
+
+           
+        }
+    }
+}
